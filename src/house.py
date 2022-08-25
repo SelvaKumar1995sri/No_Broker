6ce9ea700ef6887ed8e3097bab02e18c356c18dc
selva kumar <selvakumar1995sri@gmail.com>
2022-08-25 15:57:39 +0530

    commit completed with pytest positive tests

---
+++ src/house.py
@@ -0,0 +1,72 @@
+from urllib import request
+import uvicorn
+import pymongo
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+
+
+app=FastAPI()
+
+client=pymongo.MongoClient("mongodb+srv://gayathri:Sairambaba@cluster1.davwpcs.mongodb.net/?retryWrites=true&w=majority")
+db=client['suriya_db']
+house_collection = db['Rent_house_registry']
+
+def get_collection():
+    return house_collection
+
+class Rent_house(BaseModel):
+    house_id:int
+    place:str
+    squarefeet:int
+    type:str
+    rent:float
+
+class House_list(BaseModel):
+    data : List [Rent_house]
+
+def house_list_serializer(houselist):
+    return [house.dict() for house in houselist]
+
+    
+@app.get('/api/view_all',tags=['No_broker'])
+def view_all():
+    try:
+        collection = get_collection()
+        curser = list(collection.find({},{"_id":0}))
+        return {"data":curser}
+    except Exception as e:
+        print("error on viewing all data :" +str(e))
+
+@app.get('/api/view/{house_id}',tags=['No_broker'])
+def view(house_id):
+    try:
+        collection = get_collection()
+        curser = collection.find_one({"house_id":int(house_id)},{"_id":0})
+        return {"data":curser}
+    except Exception as e:
+        print("error on viewing data :" +str(e))
+
+@app.post('/api/add_house',tags=['No_broker'])
+def add(house : Rent_house):
+    try:
+        collection = get_collection()
+        resp = collection.insert_one(house.dict())
+        return {"status": "House details added successfully"}   
+    except Exception as e:
+        print("error on adding:" +str(e))
+
+@app.post('/api/add_house_list',tags=['No_broker'])
+def add_many(house : House_list):
+    try:
+
+        collection = get_collection()
+        house_list = house_list_serializer(house.data)
+        collection.insert_many(house_list)
+        return {"status":200}
+    except Exception as e:
+        print("error on adding list :" +str(e))
+
+if __name__ == ('__main__'):
+    uvicorn.run("house:app",reload=True)
---
+++ tests/test_house.py
@@ -0,0 +1,55 @@
+from telnetlib import STATUS
+from unittest.mock import patch
+import pytest
+import unittest
+
+from src.house import add_many, view, view_all, add
+from src.house import Rent_house,House_list
+
+class Testmongodb():
+
+    @pytest.fixture(autouse=True)
+    def mongo_get(self, mongodb):
+        resp = mongodb.list_collection_names()
+        self.mongodb = mongodb
+
+    def test_view(self):
+        with patch('src.house.get_collection') as mock_mongo:
+            mock_mongo.return_value = self.mongodb.mycollection
+            resp = view(1)
+            assert resp
+
+    # def test_fail_to_view(self):
+    #     with patch('src.house.get_collection') as mock_mongo:
+    #         mock_mongo.return_value = self.mongodb.mycollection
+    #         resp = view(3)
+    #         if not resp:
+    #             pytest.fail(f"error on adding list : {resp}")
+    #         assert resp 
+
+    def test_view_all(self):
+        with patch('src.house.get_collection') as mock_mongo:
+            mock_mongo.return_value = self.mongodb.mycollection
+            resp = view_all()
+            assert resp
+
+    def test_add(self):
+        with patch('src.house.get_collection') as mock_mongo:
+            mock_mongo.return_value = self.mongodb.mycollection
+            d = {"house_id": 3, "place": "selaiyur",
+                 "squarefeet": 1200, "type": "3bhk", "rent": 18000}
+            resp = add(d)
+            assert resp
+
+    def test_add_many(self):
+        with patch('src.house.get_collection') as mock_mongo:
+            mock_mongo.return_value = self.mongodb.mycollection
+            d = [{"house_id": 4, "place": "medavakkam",
+                 "squarefeet": 1200, "type": "3bhk", "rent": 18000},
+                 {"house_id": 5, "place": "sipcot",
+                 "squarefeet": 1200, "type": "3bhk", "rent": 18000}
+            ]
+            houselist = House_list(data=d)
+            resp = add_many(house=houselist)
+            
+            assert resp == {"status":200}
